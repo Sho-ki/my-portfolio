7a26ff4e775fdf19b1af8f30adc5506cd479146e
sho-ki <shoki0116.highjump@gmail.com>
2021-11-08 11:24:48 +0900

    add experiences part

--- components/AboutMe/Experiences.js
+++ components/AboutMe/Experiences.js
@@ -1,11 +1,11 @@
-import { Typography } from '@mui/material';
+import { Link, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 
 export function Experiences() {
   return (
     <>
       <Box lineHeight='2em' textAlign='left' marginLeft='3em'>
-        <Typography>Several of my achievements I have done included :  </Typography> <br />
+        <Typography>Several of my works I have done included :  </Typography> <br />
         <Typography>
           【 Articles 】 - Always write articles and summarize new ideas and new techniques I found{' '}
         </Typography>
@@ -15,10 +15,19 @@ export function Experiences() {
           self-development
         </Typography>
         <br />
-        {/* <Typography>
-          {' '}
-          もう一つか二つくらい何かを書く(OSS, Testing, Architecture )
-        </Typography> */}
+        <Typography>
+          【 Code Review 】 - Reviewed each other's code with other developers and did some pair programming.
+          And from design to development, I practiced using github in a way that was close to the actual
+          practice with them. <br />
+          This is what I am currently working on:{' '}
+          <Link href={'https://github.com/Sho-ki/fitness-beef'} color='secondary'>
+            Workout App(Frontend)
+          </Link>
+          {' & '}
+          <Link href={'https://github.com/Sho-ki/api.fitness-chicken'} color='secondary'>
+            Workout App(Backend)
+          </Link>
+        </Typography>
       </Box>
     </>
   );
